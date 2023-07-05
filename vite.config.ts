f7664e5e006098db1e02a7b2f5f2bde2afdbb361
Anthony Pillot <pillot.anthony@gmail.com>
2023-07-05 13:59:07 +0200

    🎨 `Sass`/`Scss` installation and format all files

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  /* Its define the global scss variables for all components
+   * to avoid import in each component
+   */
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "@/assets/_variables.scss";
+        `
+      }
     }
   }
 })
