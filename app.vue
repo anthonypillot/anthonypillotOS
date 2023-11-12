cd2673fd5866db58dfec8c9a806c158c1e96e5a8
Anthony Pillot <pillot.anthony@gmail.com>
2023-11-12 20:23:58 +0100

    ✨ Header and Main content with logo

--- app.vue
+++ app.vue
@@ -1,5 +1,54 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Header />
+  <main class="main">
+    <div class="main__center-content">
+      <img
+        class="main__center-content__logo"
+        src="https://raw.githubusercontent.com/anthonypillot/assets/main/logo/svg/logo_anthonypillotOS_white.svg"
+      />
+      <div>Anthony Pillot</div>
+      <div class="main__center-content__description">
+        <div>Software Engineer</div>
+        <div>Lille, France</div>
+      </div>
+    </div>
+  </main>
 </template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  height: 90vh;
+  width: 100%;
+
+  &__center-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    border: 5px solid;
+    border-image: linear-gradient(to left, $secondary-color, #d53a9d) 5;
+
+    padding: 1.5rem 2rem 3rem 2rem;
+
+    &__logo {
+      height: 200px;
+      width: 250px;
+    }
+
+    &__description {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      margin: 1rem 0 0 0;
+    }
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  app: {
+    head: {
+      title: "anthonypillotOS - Personal portfolio",
+      meta: [
+        {
+          name: "description",
+          content: "Personal portfolio of Anthony Pillot, Software Engineer",
+        },
+      ],
+    },
+  },
+
+  css: ["@/assets/scss/main.scss"],
+
+  vite: {
+    build: {
+      cssCodeSplit: false,
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/_variables.scss" as *;',
+        },
+      },
+    },
+  },
+
+  devtools: { enabled: true },
+});
