3b3d6d63070787c2c39aa10f1fb0070180199f82
Anthony Pillot <pillot.anthony@gmail.com>
2024-01-30 20:41:54 +0100

    perf: refactor logger messages

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -9,42 +9,42 @@ class Logger {
 
   log(message: string, args?: any): void {
     if (this.level !== "silent") {
-      const log = `[${new Date().toISOString()}] ${message}${args ? ` ${args}` : ""}`;
+      const log = `${message}${args ? ` ${args}` : ""}`;
       console.log(log);
     }
   }
 
   info(message: string, args?: any): void {
     if (this.level !== "silent") {
-      const log = `[${new Date().toISOString()}] ${`ℹ ${message}`}${args ? ` ${args}` : ""}`;
+      const log = `${chalk.blue("ℹ")} ${message}${args ? ` ${args}` : ""}`;
       console.log(log);
     }
   }
 
   success(message: string, args?: any): void {
     if (this.level !== "silent") {
-      const log = `[${new Date().toISOString()}] ${chalk.green(`✔ ${message}`)}${args ? ` ${args}` : ""}`;
+      const log = `${chalk.green("✔")} ${message}${args ? ` ${args}` : ""}`;
       console.log(log);
     }
   }
 
   start(message: string, args?: any): void {
     if (this.level !== "silent") {
-      const log = `[${new Date().toISOString()}] ${chalk.blue(`▸ ${message}`)}${args ? ` ${args}` : ""}`;
+      const log = `${chalk.blue("▸")} ${message}${args ? ` ${args}` : ""}`;
       console.log(log);
     }
   }
 
   error(message: string, args?: any): void {
     if (this.level !== "silent") {
-      const log = `[${new Date().toISOString()}] ${chalk.red(`✖ ${message}`)}${args ? ` ${args}` : ""}`;
+      const log = `${chalk.red(`✖ ${message}`)}${args ? ` ${args}` : ""}`;
       console.log(log);
     }
   }
 
   warn(message: string, args?: any): void {
     if (this.level !== "silent") {
-      const log = `[${new Date().toISOString()}] ${chalk.yellow(`⚠ ${message}`)}${args ? ` ${args}` : ""}`;
+      const log = `${chalk.yellow(`⚠ ${message}`)}${args ? ` ${args}` : ""}`;
       console.log(log);
     }
   }
@@ -52,7 +52,7 @@ class Logger {
   debug(message: string, args?: any): void {
     if (this.level !== "silent") {
       if (this.level === "debug") {
-        const log = `[${new Date().toISOString()}] ${chalk.gray(`✏ ${message}`)}${args ? ` ${args}` : ""}`;
+        const log = `${chalk.gray("✏")}${message}${args ? ` ${args}` : ""}`;
         console.log(log);
       }
     }
