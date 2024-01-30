974b9db492b086a9c738c237c718a45f701202e0
Anthony Pillot <pillot.anthony@gmail.com>
2024-01-30 20:43:23 +0100

    perf: add bootstrap plugin to log version on startup

---
+++ server/plugins/bootstrap.ts
@@ -0,0 +1,6 @@
+import { version } from "@/package.json";
+import { logger } from "@/server/utils/logger";
+
+export default defineNitroPlugin((nitro) => {
+  logger.start(`Starting [${useRuntimeConfig().app.website.title}] with version [${version}]`);
+});
