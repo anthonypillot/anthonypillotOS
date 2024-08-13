78daf6755928444d77498d413f3d59016286b87b
Anthony Pillot <pillot.anthony@gmail.com>
2024-08-13 14:40:38 +0200

    perf: reorganize ready health endpoint

--- server/plugins/bootstrap.server.ts
+++ server/plugins/bootstrap.server.ts
@@ -1,8 +1,5 @@
 import { version } from "@/package.json";
 import { convertConsoleLogToCustomLogger, logger } from "@/server/utils/logger";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 /**
  * Initializes all the necessary configurations and checks before starting the server.
@@ -16,23 +13,4 @@ export default defineNitroPlugin(async (nitro) => {
     }]`
   );
   if (process.env.LOG_LEVEL === "debug") logger.debug("Debug logging is enabled");
-  await checkDatabaseConnection();
 });
-
-/**
- * Checks the database connection and logs the result.
- * If the environment variable CI is set, the function skips the connection check.
- */
-async function checkDatabaseConnection(): Promise<void> {
-  if (!process.env.CI) {
-    try {
-      await prisma.$connect();
-      logger.success(`Connected to the database: [${process.env.POSTGRES_DATABASE}]`);
-      await prisma.$disconnect();
-    } catch (error: any) {
-      logger.error(`Failed to connect to the database: [${error.message}]`);
-    }
-  } else {
-    logger.info("CI environment detected, skipping database connection check");
-  }
-}
