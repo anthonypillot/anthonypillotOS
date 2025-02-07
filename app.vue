d5b20198a1aca23fba4fddca25d5416ce92ffc85
Anthony Pillot <pillot.anthony@gmail.com>
2025-02-07 19:59:34 +0100

    refactor(task-holdem): remove AppLoader component and related loading logic for cleaner structure

--- app.vue
+++ app.vue
@@ -4,7 +4,6 @@
 
   <!-- Main content -->
   <main class="flex flex-col gap-y-16" style="min-height: 75dvh">
-    <AppLoader v-if="loading" />
     <NuxtPage />
   </main>
 
@@ -13,19 +12,5 @@
 </template>
 
 <script setup lang="ts">
-const loading = ref<boolean>(true);
-
-/**
- * Used to show/hide the loading indicator,
- * by listening to the Nuxt app events.
- */
-const nuxtApp = useNuxtApp();
-nuxtApp.hook("page:start", () => {
-  loading.value = true;
-});
-nuxtApp.hook("page:finish", () => {
-  loading.value = false;
-});
-
 useSeo();
 </script>
