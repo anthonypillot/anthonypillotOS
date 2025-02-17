f8c37292609e794d650b654c09861ed403d29ea3
Anthony Pillot <pillot.anthony@gmail.com>
2025-02-17 20:30:19 +0100

    fix: improve error logging for Nuxt errors
    and no longer log 404 error

--- server/plugins/error-handler.server.ts
+++ server/plugins/error-handler.server.ts
@@ -1,9 +1,15 @@
+import { H3Error } from "h3";
+
 export default defineNitroPlugin((nitro) => {
   nitro.hooks.hook("error", async (error, { event }) => {
-    if (event) {
-      logger.error(`[${event.path}] Application error:`, error);
+    if (error instanceof H3Error) {
+      if (error.statusCode !== 404 && event) {
+        logger.error(`Nuxt error on path [${event.path}], error: ${error}`);
+      } else if (error.statusCode !== 404) {
+        logger.error(`Nuxt error: ${error}`);
+      }
     } else {
-      logger.error("Application error:", error);
+      logger.error(`Application error: ${error}`);
     }
   });
 });
