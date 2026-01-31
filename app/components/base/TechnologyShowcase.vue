4152569c19c4746d45094e9fa30dc70346296642
Anthony Pillot <pillot.anthony@gmail.com>
2026-01-31 18:32:01 +0000

    perf: add animated counter component to Technology Showcase

--- app/components/base/TechnologyShowcase.vue
+++ app/components/base/TechnologyShowcase.vue
@@ -7,10 +7,20 @@
       <p class="mt-2 text-lg leading-8 text-gray-200">All the technologies I have used during my career.</p>
       <div class="mt-4 flex flex-wrap gap-4">
         <div class="bg-gray-500/5 rounded-xl size-60 flex flex-col place-items-center justify-center text-center">
-          <p class="text-2xl text-white">{{ yearsOfExperience }} <span class="text-indigo-400">years</span></p>
+          <p class="text-2xl text-white">
+            <ClientOnly>
+              <BaseCounterAnimation :from="0" :to="yearsOfExperience" :duration="10" :start-when="true" />
+              <span class="text-indigo-400"> years</span>
+            </ClientOnly>
+          </p>
         </div>
         <div class="bg-gray-500/5 rounded-xl size-60 flex flex-col place-items-center justify-center text-center">
-          <p class="text-2xl text-white">{{ icons?.length }}+ <span class="text-indigo-400">technologies</span></p>
+          <p class="text-2xl text-white">
+            <ClientOnly>
+              <BaseCounterAnimation :from="0" :to="numberOfTechnologies" :duration="10" :start-when="true" />+
+              <span class="text-indigo-400">technologies</span>
+            </ClientOnly>
+          </p>
         </div>
       </div>
       <div class="mt-4 flex flex-col gap-4">
@@ -38,4 +48,6 @@ const { data } = await useFetch("/api/icons", {
 });
 
 const icons = data.value;
+
+const numberOfTechnologies = icons?.length || 30;
 </script>
