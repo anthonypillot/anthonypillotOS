7091246019f832c75e30190b66031a22bbb43810
Anthony Pillot <anthony.pillot@ikmail.com>
2026-06-13 14:42:23 +0200

    fix: remove text-center in history cleaner

--- app/components/github/HistoryCleaner.vue
+++ app/components/github/HistoryCleaner.vue
@@ -112,10 +112,10 @@
   <UModal v-model:open="confirmationModal" :dismissible="!loading" title="Confirm deletion" :ui="{ footer: 'justify-end' }">
     <template #body>
       <div class="sm:flex sm:items-start">
-        <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+        <div class="mx-auto flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
           <UIcon name="i-heroicons-exclamation-triangle" class="h-6 w-6 text-red-600" />
         </div>
-        <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+        <div class="mt-3 sm:ml-4 sm:mt-0 sm:text-left">
           <p class="text-sm text-muted">
             Are you sure you want to delete the history of your GitHub account
             <span class="font-semibold">{{ form.account }}</span>
