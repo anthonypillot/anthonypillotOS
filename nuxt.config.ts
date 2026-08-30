c8be35ed5bbe4433db561625b9d88551c7271d9a
Anthony Pillot <anthony.pillot@ikmail.com>
2026-08-30 15:26:45 +0200

    perf: remove Matomo analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,7 +52,7 @@ export default defineNuxtConfig({
 
   vite: {
     optimizeDeps: {
-      include: ["@vue/devtools-core", "@vue/devtools-kit", "gsap", "matter-js", "vue-matomo"],
+      include: ["@vue/devtools-core", "@vue/devtools-kit", "gsap", "matter-js"],
     },
   },
 
